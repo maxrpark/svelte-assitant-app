19f74cf98d727eab0b792bab282243ad0c898afd
Maxigong <maxi@nolgong.com>
2022-03-18 22:38:04 +0900

    created

---
+++ src/components/Footer.svelte
@@ -0,0 +1,26 @@
+<script>
+  let year = new Date().getFullYear();
+</script>
+
+<footer>
+  <p>
+    Made with Vue, by
+    <a
+      target="_blank"
+      rel="noreferrer"
+      href="https://twitter.com/MaxCodeJourney"
+    >
+      Maxi Ruti,
+    </a>{year}
+  </p>
+  <p>
+    Visit my other projects
+    <a
+      target="_blank"
+      rel="noreferrer"
+      href="https://my-portfolio-blog-website.netlify.app/"
+    >
+      My Portfolio Website
+    </a>
+  </p>
+</footer>
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { Router, Link } from 'svelte-navigator';
+</script>
+
+<Router>
+  <nav class="nav">
+    <div class="navbar">
+      <Link to="/" class="logo">assistant</Link>
+      <Link to="/todo">Todo</Link>
+    </div>
+  </nav>
+</Router>
